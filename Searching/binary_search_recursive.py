43d3da85b595e6c9cf9b5a81439a4450c7e838e0
Udit Kalra <80574577+Udit-Kalra-1117@users.noreply.github.com>
2023-08-04 13:42:47 +0530

    Update binary_search_recursive.py

--- Searching/binary_search_recursive.py
+++ Searching/binary_search_recursive.py
@@ -1,3 +1,15 @@
+# Binary Search
+# Binary search is very fast and efficient searching algorithm. 
+# It requires to be list be in a sorted order, ie; either in ascending or descending.
+# In this method, to search an element you might compare that respective element present in the center of the list 
+# and if it’s same then the search is successfully finished and if not, then the list is  divided into two parts:
+# one from 0th element to the centered element and other from the centered element to the last element.
+
+# Time Complexity (Best)	Ω(1)
+# Time Complexity (Avg)	Θ(log n)
+# Time Complexity (Worst)	O(log n)
+# Space Complexity	O(1)
+
 def binarySearch(a, i, l, h):
     if h >= l:
         m = l + (h - l)//2
